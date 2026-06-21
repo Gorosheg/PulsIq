820314a5eb267ce67ddc7e109a162a8d71bb338b
AnnaG <anutagoroxowa@yandex.ru>
2026-06-21 13:58:09 +0400

    Up App version: 2 / 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         applicationId = "gorosheg.pulsiq"
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         vectorDrawables {
             useSupportLibrary = true
